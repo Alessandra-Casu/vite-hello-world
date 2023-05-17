40f35dcc182d6c5f907f2b53151cf3596d2a5b8b
Alessandra-Casu <alessandracasu919@gmail.com>
2023-05-17 17:57:14 +0200

    add components

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+    //qui va il codice JavaScript
+    import HelloWorld from './components/HelloWorld.vue';
+    import Jumbotron from './components/Jumbotron.vue';
+</script>
+
+<template>
+  <section>
+    <!--qui va il nostro HTML-->
+   <HelloWorld></HelloWorld>
+  </section>
+  <section>
+    <!--qui va il nostro HTML-->
+    <Jumbotron></Jumbotron>
+  </section>
+  
+</template>
+
+<style scoped>
+    /*qui aggiungeremo CSS*/
+ 
+</style>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="box">
+    <p>Ciao io sono Jumbotron!</p>
+  </div>
+</template>
+
+<style scoped>
+    .box{
+        width: 50%;
+        margin: auto;
+        padding: 2rem;
+        color: black;
+        font-weight: 800;
+        background-color: grey;
+        border-radius: 1rem; 
+    }
+</style>
