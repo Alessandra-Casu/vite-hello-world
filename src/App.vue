8742be87b3ba80e9a3abb246b4e7a92400ecdeaf
Alessandra-Casu <alessandracasu919@gmail.com>
2023-05-18 10:58:46 +0200

    add export

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
-<script setup>
-    //qui va il codice JavaScript
-    import HelloWorld from './components/HelloWorld.vue';
-    import Jumbotron from './components/Jumbotron.vue';
+<script>
+//qui va il codice JavaScript
+
+import HelloWorld from "./components/HelloWorld.vue";
+import Jumbotron from "./components/Jumbotron.vue";
+
+export default {
+  components: {
+    HelloWorld,
+    Jumbotron,
+  },
+};
 </script>
 
 <template>
   <section>
     <!--qui va il nostro HTML-->
-   <HelloWorld></HelloWorld>
+    <HelloWorld></HelloWorld>
   </section>
   <section>
     <!--qui va il nostro HTML-->
     <Jumbotron></Jumbotron>
   </section>
-  
 </template>
 
 <style scoped>
-    /*qui aggiungeremo CSS*/
- 
+/*qui aggiungeremo CSS*/
 </style>
